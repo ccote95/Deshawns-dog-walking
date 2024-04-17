0855afb06ef46728ac0c7a1369de5fd71d3a58c8
Chad Cote <chadcote58@gmail.com>
2024-04-17 15:54:18 -0400

    created a modal to render when the user clicks add a dog

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { getDog, getGreeting } from "./apiManager";
 import { useEffect, useState } from "react";
-
+import"./Home.css";
 export default function Home() {
   const [greeting, setGreeting] = useState({
     message: "Not Connected to the API",
   });
   const [dog, setDog] = useState([])
+  const [showModal, setShowModal]= useState(false)
+  const [newDog, setNewDog]=useState("")
 
   useEffect(() => {
     getGreeting()
@@ -23,10 +25,23 @@ export default function Home() {
   return (
     <>
     <p>{greeting.message}</p>
+    <div className="add-dog">
+      <button onClick={() => setShowModal(true)}>Add a Dog</button>
+    </div>
+    <div>
         {dog.map((dog) =>
       <p key = {dog.id}><Link to ={`/${dog.id}`}  style={{ textDecoration: "none", color: "black" }}>{dog.name}</Link></p>
       )}
-
+      </div>
+        {showModal && (
+        <div>
+          <div>
+            <span className="close" onClick ={() => setShowModal(false)}>&times;</span>
+            <h2>Add a new dog!</h2>
+            <input placeholder="Enter the new dogs name." type="text"/>
+          </div>
+        </div>
+        )}
     </>
 
   );
