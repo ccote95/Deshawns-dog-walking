5e6fbe6d9bfdd0a1e1f1e494c544e48739716819
Chad Cote <chadcote58@gmail.com>
2024-04-25 15:40:57 -0400

    Created an input field and a submit button for the user to add a new city

--- client/src/CitiesList.jsx
+++ client/src/CitiesList.jsx
@@ -3,17 +3,26 @@ import { getCities } from "./apiManager.js";
 
 export const CityList = () => {
     const [cities, setCities] = useState([])
+    const [newCity, setNewCity] = useState({})
 
     useEffect(() => {
         getCities().then(setCities)
     },[]);
 
+    const handleSubmit =() => {
+
+    }
+
     return(
         <div>
             <div>
                  <div>
-                <input type="text" placeholder="Add a City" />
-                <button>Submit</button>
+                <input type="text" placeholder="Add a City" onChange={(e) => {
+                    const userCopy = {...newCity};
+                    userCopy.name = e.target.value
+                    setNewCity(userCopy);
+                }}/>
+                <button onClick={handleSubmit}>Submit</button>
                     
                  </div>
                 {cities.map(city => {
